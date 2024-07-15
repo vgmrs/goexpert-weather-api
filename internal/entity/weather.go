560380e029a7c7bdc290e9acdbaff9905fd38569
Victor Guimarães <victorfelipeguimaraes@hotmail.com>
2024-07-15 07:06:29 +0000

    Create weather entity

--- internal/entity/weather.go
+++ internal/entity/weather.go
@@ -1,1 +1,28 @@
 package entity
+
+import (
+	"fmt"
+
+	"github.com/vgmrs/goexpert-weather-api/pkg/temperature"
+)
+
+type Weather struct {
+	Celsius    string
+	Kelvin     string
+	Fahrenheit string
+}
+
+func NewWeather(c float64) *Weather {
+	k := temperature.CelsiusToKelvin(c)
+	f := temperature.CelsiusToFahrenheit(c)
+
+	celsius := fmt.Sprintf("%.2f", c)
+	kelvin := fmt.Sprintf("%.2f", k)
+	fahrenheit := fmt.Sprintf("%.2f", f)
+
+	return &Weather{
+		Celsius:    celsius,
+		Kelvin:     kelvin,
+		Fahrenheit: fahrenheit,
+	}
+}
--- pkg/temperature/converter.go
+++ pkg/temperature/converter.go
@@ -1,9 +1,9 @@
 package temperature
 
-func CelsiusToFahrenheit(c float32) float32 {
-	return (c*1.8) + 32
+func CelsiusToFahrenheit(c float64) float64 {
+	return (c * 1.8) + 32
 }
 
-func CelsiusToKelvin(c float32) float32 {
+func CelsiusToKelvin(c float64) float64 {
 	return c + 273
 }
